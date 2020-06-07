1d55e2ef517529c6f79a06b7d72abf9dae184621
Filip Henningsson <erik.filip.henningsson@gmail.com>
2020-06-07 19:31:48 +0200

    Application cleanup

--- Rush-Engine/src/core/Application.cpp
+++ Rush-Engine/src/core/Application.cpp
@@ -2,23 +2,11 @@
 
 #include "Logger.h"
 
-#ifdef RUSH_OPENGL
-	#include <glad/glad.h>
-	#include <GLFW/glfw3.h>
-#endif
-
 namespace Rush {
 
 Application::Application(){
 	WindowProperties props;
 	m_Window = AbstractWindow::CreateWindow(props);
-	// Logger::Info("Initializing Glad...");
-	// if(!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)){
-	// 	Logger::Error("Failed to initialize Glad!");
-	// 	return false;
-	// }
-	// Logger::Info("Glad initialized");
-	// Logger::Info("OpenGL initialized, version: " + std::to_string(GLVersion.major) + "." + std::to_string(GLVersion.minor));
 }
 
 Application::~Application(){
