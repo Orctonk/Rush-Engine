470aa2639e0ef7b5bad1dd6469d88dfed0d53ab3
Filip Henningsson <erik.filip.henningsson@gmail.com>
2020-06-14 20:24:58 +0200

    Added texture to interface

--- Rush-Engine/src/Rush.h
+++ Rush-Engine/src/Rush.h
@@ -10,6 +10,7 @@
 #include "Rush/graphics/VertexBuffer.h"
 #include "Rush/graphics/VertexArray.h"
 #include "Rush/graphics/Shader.h"
+#include "Rush/graphics/Texture.h"
 
 #include "Rush/events/EventManager.h"
 #include "Rush/events/Event.h"
