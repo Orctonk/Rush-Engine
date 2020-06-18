f77045b763eac34f9fe7101ccd0d15c54a9d955b
Filip Henningsson <erik.filip.henningsson@gmail.com>
2020-06-18 10:34:00 +0200

    Added a SceneLayer skeleton

---
+++ Rush-Engine/src/Rush/scene/SceneLayer.cpp
@@ -0,0 +1,29 @@
+#include "SceneLayer.h"
+
+namespace Rush {
+
+SceneLayer::SceneLayer(){}
+SceneLayer::~SceneLayer(){}
+
+void SceneLayer::OnAttach() {
+    
+}
+
+void SceneLayer::OnDetach() {
+
+}
+
+void SceneLayer::OnUpdate() {
+
+}
+
+void SceneLayer::OnEvent(Event &e) {
+
+}
+
+void SceneLayer::OnImguiRender() {
+
+}
+
+
+}
