10c4db42033351c6b3582876b39c4776768a065a
Filip Henningsson <c18fhn@cs.umu.se>
2020-06-25 23:28:12 +0200

    Created more components and added default initialization

--- Rush-Engine/src/Rush/scene/Light.h
+++ Rush-Engine/src/Rush/scene/Light.h
@@ -11,35 +11,35 @@ enum class RUSH_API LightType {
 };
 
 struct RUSH_API DirectionalLight {
-    glm::vec3 direction;
+    glm::vec3 direction {0.0f,0.0f,0.0f};
 
-    glm::vec3 ambient;
-    glm::vec3 diffuse;
-    glm::vec3 specular;
+    glm::vec3 ambient {0.1f,0.1f,0.1f};
+    glm::vec3 diffuse {1.0f,1.0f,1.0f};
+    glm::vec3 specular{1.0f,1.0f,1.0f};
 };
 
 struct RUSH_API PointLight {
-    glm::vec3 position;
+    glm::vec3 position  {0.0f,0.0f,0.0f};
 
-    float constant;
-    float linear;
-    float quadratic;
+    float constant  {0.7f};
+    float linear    {0.07f};
+    float quadratic {0.02f};
 
-    glm::vec3 ambient;
-    glm::vec3 diffuse;
-    glm::vec3 specular;
+    glm::vec3 ambient {0.1f,0.1f,0.1f};
+    glm::vec3 diffuse {1.0f,1.0f,1.0f};
+    glm::vec3 specular{1.0f,1.0f,1.0f};
 };
 
 struct RUSH_API Spotlight {
-    glm::vec3 position;  
-    glm::vec3 direction;
+    glm::vec3 position {0.0f,0.0f,0.0f};  
+    glm::vec3 direction{0.0f,0.0f,0.0f};
 
     float cutOff;
     float outerCutOff;
   
-    glm::vec3 ambient;
-    glm::vec3 diffuse;
-    glm::vec3 specular;
+    glm::vec3 ambient {0.1f,0.1f,0.1f};
+    glm::vec3 diffuse {1.0f,1.0f,1.0f};
+    glm::vec3 specular{1.0f,1.0f,1.0f};
 };
 
 #endif // __LIGHT_H__
---
+++ Rush-Engine/src/Rush/scene/Mesh.h
@@ -0,0 +1,7 @@
+#include "Rush/core/Core.h"
+#include "Rush/graphics/VertexArray.h"
+
+
+struct Mesh {
+    Rush::Unique<Rush::VertexArray> vertices;
+};
