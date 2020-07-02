b0d7c4dfeb8562ac6cb48737879bbbd95e8a1f3e
Filip Henningsson <erik.filip.henningsson@gmail.com>
2020-07-02 12:44:36 +0200

    Moved ImGui implementation into its own file

---
+++ Rush-Engine/src/Rush/imgui/ImguiImpl.cpp
@@ -0,0 +1,7 @@
+#define IMGUI_IMPL_OPENGL_LOADER_GLAD
+#include <imgui.cpp>
+#include <imgui_demo.cpp>
+#include <imgui_draw.cpp>
+#include <imgui_widgets.cpp>
+#include <misc/cpp/imgui_stdlib.cpp>
+#include <examples/imgui_impl_opengl3.cpp>
