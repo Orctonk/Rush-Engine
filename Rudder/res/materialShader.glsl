258b3f671edfce1e88427d5af207526d792eef2d
Filip Henningsson <erik.filip.henningsson@gmail.com>
2020-07-07 15:41:42 +0200

    Fixed normal mapping

--- Rudder/res/materialShader.glsl
+++ Rudder/res/materialShader.glsl
@@ -11,17 +11,14 @@ uniform mat4 u_ViewProjection;
 out vec3 FragPos; // output a color to the fragment shader
 out vec2 uv;
 out mat3 TBN;
-out vec3 normal;
 
 void main()
 {
     FragPos = vec3(u_Model * vec4(aPos, 1.0)); 
     uv = uvPos;
 
-    normal = normalize(mat3(transpose(inverse(u_Model))) * aNormal);
-
-    vec3 T = normalize(mat3(transpose(inverse(u_Model))) * aTangent);;
-    vec3 N = normalize(mat3(transpose(inverse(u_Model))) * aNormal);;
+    vec3 T = normalize(vec3(u_Model * vec4(aTangent,0.0)));
+    vec3 N = normalize(vec3(u_Model * vec4(aNormal,0.0)));
     vec3 B = cross(N,T);
     TBN = mat3(T,B,N);
     gl_Position = u_ViewProjection * u_Model * vec4(aPos, 1.0);
@@ -44,7 +41,6 @@ layout (location = 2) out vec4 gColor;
 in vec3 FragPos;
 in vec2 uv;
 in mat3 TBN;
-in vec3 normal;
 
 uniform vec3 u_CamPos;
 uniform Material u_Material;
@@ -52,10 +48,8 @@ uniform Material u_Material;
 void main() {   
     gPosition = vec4(FragPos,1.0);
 
-    // vec3 normal = texture(u_Material.normal,uv).rgb;
-    // normal = normal * 2 - 1;
-    // gNormal = vec4(TBN * normal, 1.0);
-    gNormal = vec4(normal,1.0);
+    vec3 normal = texture(u_Material.normal,uv).rgb;
+    gNormal = vec4(normalize(TBN * normal), 1.0);
 
     gColor = vec4(texture(u_Material.diffuse,uv).rgb,texture(u_Material.specular,uv).r);
 }
