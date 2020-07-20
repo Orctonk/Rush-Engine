798450f033a68fcd7298d8dd3a3df22263b3d6db
Filip Henningsson <erik.filip.henningsson@gmail.com>
2020-07-20 21:12:31 +0200

    Implemented basic mesh instancing

---
+++ Rush-Engine/src/Rush/resources/RootMesh.h
@@ -0,0 +1,12 @@
+#ifndef __ROOTMESH_H__
+#define __ROOTMESH_H__
+
+#include "Rushpch.h"
+#include "SubMesh.h"
+
+struct RootMesh {
+    std::string name;
+    std::vector<SubMesh> submeshes;
+};
+
+#endif // __ROOTMESH_H__
---
+++ Rush-Engine/src/Rush/resources/SubMesh.h
@@ -0,0 +1,15 @@
+#ifndef __SUBMESH_H__
+#define __SUBMESH_H__
+
+#include <Rushpch.h>
+#include "Rush/core/Core.h"
+#include "Rush/graphics/VertexArray.h"
+#include "Material.h"
+
+struct RUSH_API SubMesh {
+    std::string meshName;
+    Rush::Unique<Rush::VertexArray> vertices;
+    Rush::Shared<Rush::Material> material;
+};
+
+#endif // __SUBMESH_H__
