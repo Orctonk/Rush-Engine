35f283ffeffc62ce0fac34d2136145618b0ae087
Filip Henningsson <erik.filip.henningsson@gmail.com>
2020-08-26 14:23:35 +0200

    Added render sorting to enable transparent materials

--- Rush-Engine/src/Rush/graphics/Renderer.cpp
+++ Rush-Engine/src/Rush/graphics/Renderer.cpp
@@ -5,8 +5,7 @@
 namespace Rush{
 
 Unique<RenderingAPI> Renderer::s_API = RenderingAPI::Create();
-glm::mat4 Renderer::s_SceneVP = glm::mat4();
-glm::vec3 Renderer::s_CameraPos = glm::vec3();
+Renderer::RenderData Renderer::s_Data = Renderer::RenderData(); 
 RenderStats Renderer::s_Stats = RenderStats();
 
 void Renderer::Init(){
@@ -22,15 +21,71 @@ void Renderer::OnResize(uint32_t width, uint32_t height){
     s_API->SetViewport(0,0,width, height);
 }
 
-void Renderer::BeginScene(Camera &camera, glm::mat4 &view){
+void Renderer::BeginScene(Camera &camera, glm::mat4 &view, Shared<Shader> shaderOverride){
     RUSH_PROFILE_FUNCTION();
-    s_SceneVP = camera.GetProjection() * glm::inverse(view);
-    s_CameraPos = glm::vec3(view * glm::vec4(0.0f,0.0f,0.0f,1.0f));
+    s_Data.sceneVP = camera.GetProjection() * glm::inverse(view);
+    s_Data.cameraPos = glm::vec3(view * glm::vec4(0.0f,0.0f,0.0f,1.0f));
+    s_Data.shaderOverride = shaderOverride;
 }
 
 void Renderer::EndScene(){
     RUSH_PROFILE_FUNCTION();
 
+    std::sort(s_Data.keys.begin(),s_Data.keys.end(),[](RenderableKey &k1, RenderableKey &k2){
+        if(k1.sortParam & (1UL<<63)){ // K1 Transparent
+            if (k2.sortParam & (1UL<<63))
+                return k1.sortParam > k2.sortParam;
+            return false;
+        } else { // K1 Opaque
+            return k1.sortParam < k2.sortParam;
+        }
+    });
+
+    s_API->SetOption(PolygonMode::Fill);
+    s_API->SetOption(CullFace::Clockwise);
+    s_API->SetOption(DepthTest::Less);
+    s_API->SetOption(BlendMode::None);
+
+    bool switched = false;
+
+    for(auto key: s_Data.keys){
+        auto &material = s_Data.submittedRenderables[key.renderableIndex].material;
+        auto &model = s_Data.submittedRenderables[key.renderableIndex].model;
+        auto &va = s_Data.submittedRenderables[key.renderableIndex].va;
+        if(!switched && (key.sortParam & (1UL << 63))){
+            s_API->SetOption(BlendMode::Add);
+            switched = true;
+        }
+        if(s_Data.shaderOverride == nullptr){
+            material->Bind();
+            material->materialShader->SetUniform("u_Scene.viewProjection", ShaderData::MAT4, glm::value_ptr(s_Data.sceneVP));
+            material->materialShader->SetUniform("u_Scene.model", ShaderData::MAT4, glm::value_ptr(model));
+            material->materialShader->SetUniform("u_Scene.camPos", ShaderData::FLOAT3, glm::value_ptr(s_Data.cameraPos));
+        } else {
+            s_Data.shaderOverride->Bind();
+            s_Data.shaderOverride->SetUniform("u_Material.diffuse",0);
+            material->diffuseTexture->Bind(0);
+            s_Data.shaderOverride->SetUniform("u_Material.specular",1);
+            material->specularTexture->Bind(1);
+            s_Data.shaderOverride->SetUniform("u_Material.normal",2);
+            material->normalTexture->Bind(2);
+            s_Data.shaderOverride->SetUniform("u_Material.shininess",material->shininess);
+            s_Data.shaderOverride->SetUniform("u_Material.color",material->color);
+            s_Data.shaderOverride->SetUniform("u_Scene.viewProjection", ShaderData::MAT4, glm::value_ptr(s_Data.sceneVP));
+            s_Data.shaderOverride->SetUniform("u_Scene.model", ShaderData::MAT4, glm::value_ptr(model));
+            s_Data.shaderOverride->SetUniform("u_Scene.camPos", ShaderData::FLOAT3, glm::value_ptr(s_Data.cameraPos));
+        }
+
+        s_API->DrawIndexed(va);
+
+        s_Stats.drawCallCount++;
+        uint32_t ic = va->GetIndexBuffer()->GetIndexCount();
+        s_Stats.primitiveCount += ic / 3;
+        s_Stats.vertexCount += ic;
+        s_Stats.programCount++;
+    }
+    s_Data.submittedRenderables.clear();
+    s_Data.keys.clear();
 }
 
 void Renderer::ResetRenderStats(){
@@ -40,9 +95,9 @@ void Renderer::ResetRenderStats(){
 void Renderer::Submit(const Shared<Shader> &shader, const Shared<VertexArray> &va,const glm::mat4 &model) {
     RUSH_PROFILE_FUNCTION();
     shader->Bind();
-    shader->SetUniform("u_Scene.viewProjection", ShaderData::MAT4, glm::value_ptr(s_SceneVP));
+    shader->SetUniform("u_Scene.viewProjection", ShaderData::MAT4, glm::value_ptr(s_Data.sceneVP));
     shader->SetUniform("u_Scene.model", ShaderData::MAT4, glm::value_ptr(model));
-    shader->SetUniform("u_Scene.camPos", ShaderData::FLOAT3, glm::value_ptr(s_CameraPos));
+    shader->SetUniform("u_Scene.camPos", ShaderData::FLOAT3, glm::value_ptr(s_Data.cameraPos));
     s_API->SetOption(PolygonMode::Fill);
     s_API->SetOption(CullFace::None);
     s_API->SetOption(DepthTest::LessOrEqual);
@@ -54,6 +109,56 @@ void Renderer::Submit(const Shared<Shader> &shader, const Shared<VertexArray> &v
     s_Stats.primitiveCount += ic / 3;
     s_Stats.vertexCount += ic;
     s_Stats.programCount++;
+}
+
+void Renderer::Submit(MaterialInstance material, const Shared<VertexArray> &va,const glm::mat4 &model) {
+    RUSH_PROFILE_FUNCTION();
+    s_Data.submittedRenderables.push_back({model,material.parent,va});
+    uint64_t sortParam = material.parent->mode == RenderingMode::Opaque ? 0 : (1UL << 63);
+    float distance = glm::distance(glm::vec3(model[3]),s_Data.cameraPos);
+    sortParam |= ((uint64_t)(distance * 100) & 0x7FFFFFFFFFFFFFFF);
+    s_Data.keys.push_back({
+        sortParam,
+        s_Data.submittedRenderables.size() - 1
+    });
+    // if(s_Data.shaderOverride == nullptr){
+    //     material.parent->Bind();
+    //     material.parent->materialShader->SetUniform("u_Scene.viewProjection", ShaderData::MAT4, glm::value_ptr(s_Data.sceneVP));
+    //     material.parent->materialShader->SetUniform("u_Scene.model", ShaderData::MAT4, glm::value_ptr(model));
+    //     material.parent->materialShader->SetUniform("u_Scene.camPos", ShaderData::FLOAT3, glm::value_ptr(s_Data.cameraPos));
+    // } else {
+    //     s_Data.shaderOverride->Bind();
+    //     s_Data.shaderOverride->SetUniform("u_Material.diffuse",0);
+    //     material.parent->diffuseTexture->Bind(0);
+    //     s_Data.shaderOverride->SetUniform("u_Material.specular",1);
+    //     material.parent->specularTexture->Bind(1);
+    //     s_Data.shaderOverride->SetUniform("u_Material.normal",2);
+    //     material.parent->normalTexture->Bind(2);
+    //     s_Data.shaderOverride->SetUniform("u_Material.shininess",material.parent->shininess);
+    //     s_Data.shaderOverride->SetUniform("u_Material.color",material.parent->color);
+    //     s_Data.shaderOverride->SetUniform("u_Scene.viewProjection", ShaderData::MAT4, glm::value_ptr(s_Data.sceneVP));
+    //     s_Data.shaderOverride->SetUniform("u_Scene.model", ShaderData::MAT4, glm::value_ptr(model));
+    //     s_Data.shaderOverride->SetUniform("u_Scene.camPos", ShaderData::FLOAT3, glm::value_ptr(s_Data.cameraPos));
+    // }
+    // s_API->SetOption(PolygonMode::Fill);
+    // s_API->SetOption(CullFace::Clockwise);
+    // if(material.parent->mode == RenderingMode::Opaque){
+    //     s_API->SetOption(DepthTest::Less);
+    //     s_API->SetOption(BlendMode::None);
+    // } else if (material.parent->mode == RenderingMode::Transparent){
+    //     s_API->SetOption(DepthTest::Less);
+    //     s_API->SetOption(BlendMode::Add);
+    // } else if (material.parent->mode == RenderingMode::Cutoff){
+    //     s_API->SetOption(DepthTest::Less);
+    //     s_API->SetOption(BlendMode::None);
+    // }
+    // s_API->DrawIndexed(va);
+
+    // s_Stats.drawCallCount++;
+    // uint32_t ic = va->GetIndexBuffer()->GetIndexCount();
+    // s_Stats.primitiveCount += ic / 3;
+    // s_Stats.vertexCount += ic;
+    // s_Stats.programCount++;
 }
 
 void Renderer::RenderCube(const Shared<Shader> &shader, const glm::mat4 &model){
--- Rush-Engine/src/Rush/graphics/Renderer.h
+++ Rush-Engine/src/Rush/graphics/Renderer.h
@@ -1,6 +1,7 @@
 #ifndef __RENDERER_H__
 #define __RENDERER_H__
 
+#include "Rush/resources/MaterialInstance.h"
 #include "Rush/scene/Camera.h"
 #include "Rush/core/Core.h"
 #include "RenderingAPI.h"
@@ -20,21 +21,40 @@ struct RUSH_API RenderStats {
 
 class RUSH_API Renderer {
 private:
+    struct RenderableKey{
+        uint64_t sortParam;
+        uint32_t renderableIndex;
+    };
+
+    struct Renderable{
+        glm::mat4 model;
+        Shared<Material> material;
+        Shared<VertexArray> va;
+    };
+
+    struct RenderData{
+        glm::mat4 sceneVP;
+        glm::vec3 cameraPos;
+        Shared<Shader> shaderOverride;
+
+        std::vector<Renderable> submittedRenderables;
+        std::vector<RenderableKey> keys;
+    };
     static Unique<RenderingAPI> s_API;
-    static glm::mat4 s_SceneVP;
-    static glm::vec3 s_CameraPos;
+    static RenderData s_Data;
     static RenderStats s_Stats;
 public:
     static void Init();
     static void Shutdown();
     static void OnResize(uint32_t width, uint32_t height);
-    static void BeginScene(Camera &camera, glm::mat4 &view);
+    static void BeginScene(Camera &camera, glm::mat4 &view, Shared<Shader> shaderOverride = nullptr);
     static void EndScene();
 
     static void ResetRenderStats();
     static RenderStats GetRenderStats(){return s_Stats;}
 
     static void Submit(const Shared<Shader> &shader, const Shared<VertexArray> &va, const glm::mat4 &model);
+    static void Submit(MaterialInstance material, const Shared<VertexArray> &va, const glm::mat4 &model);
 
     static void RenderCube(const Shared<Shader> &shader, const glm::mat4 &model);
 
