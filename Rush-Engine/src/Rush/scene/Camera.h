35f7b12234fbcd5a0cc1eac1e72cb494210435f3
Filip Henningsson <erik.filip.henningsson@gmail.com>
2020-09-01 21:20:07 +0200

    Added const qualifier to getters in camera for serialization

--- Rush-Engine/src/Rush/scene/Camera.h
+++ Rush-Engine/src/Rush/scene/Camera.h
@@ -28,20 +28,20 @@ public:
     void SetPerspective(float aspect, float FOV, float near = 0.1f, float far = 100.0f);
     void SetOrthographic(float left, float right, float top, float bottom, float near = 0.1f, float far = 100.0f);
 
-    float GetNear(){ return m_Near; }
-    float GetFar(){ return m_Far; }
+    float GetNear() const { return m_Near; }
+    float GetFar() const { return m_Far; }
 
     // Orthographic camera specific
-    float GetLeft(){ return m_LeftOrAspect; }
-    float GetRight(){ return m_RightOrFOV; }
-    float GetTop(){ return m_Top; }
-    float GetBottom(){ return m_Bottom; }
+    float GetLeft() const { return m_LeftOrAspect; }
+    float GetRight() const { return m_RightOrFOV; }
+    float GetTop() const { return m_Top; }
+    float GetBottom() const { return m_Bottom; }
 
     // Perspective camera specific
-    float GetAspect(){ return m_LeftOrAspect; }
-    float GetFOV(){ return m_RightOrFOV; }
+    float GetAspect() const { return m_LeftOrAspect; }
+    float GetFOV() const { return m_RightOrFOV; }
 
-    bool IsPerspective(){ return m_Top == m_Bottom; }
+    bool IsPerspective() const { return m_Top == m_Bottom; }
 
     glm::mat4 GetProjection() { return m_Projection; }
 };
