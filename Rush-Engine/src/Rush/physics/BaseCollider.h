9cc7fa6a9a67967f2f18e845fe64910c7829f12b
Filip Henningsson <c18fhn@cs.umu.se>
2021-03-15 14:59:30 +0100

    Added note about collision functions in base collider

--- Rush-Engine/src/Rush/physics/BaseCollider.h
+++ Rush-Engine/src/Rush/physics/BaseCollider.h
@@ -5,6 +5,10 @@
 
 #include "Rush/scene/components/TransformComponent.h"
 
+// TODO: Currently each collider type requires collision checks to be implemented for every other class. 
+// This means that there is n^2 collision functions and most are duplicated. 
+// Example: Sphere checks Sphere vs AABB and AABB checks AABB vs Sphere even though these are cleary the same function.
+
 struct RUSH_API Manifold {
 	glm::vec3 collisionNormal;
 	float penetrationDepth;
